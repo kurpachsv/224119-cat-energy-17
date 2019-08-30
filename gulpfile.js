24e777da4474099758ec3524c319145689a6447f
kurpachsv <kurpachsv@gmail.com>
2019-08-30 19:24:05 +0300

    Оптимизация сборки svg-спрайта

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task("images", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-,logo-,htmlacademy}*.svg")
+  return gulp.src("source/img/svg-sprite/*.svg")
     .pipe(cheerio({
       run: function ($) {
         $("[fill]").removeAttr("fill");
@@ -83,7 +83,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/{icon-,logo-,htmlacademy}*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/img/**/*.{png,jpg,svg}", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html").on("change", gulp.series("html", "refresh"));
 });
 
